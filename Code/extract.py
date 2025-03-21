8bbb7bfed33adaf5fe4ee3891360d73d2960e06a
Hetu <sahu.hetram@gmail.com>
2025-03-21 08:34:40 +0530

    data transftromation done

--- Code/extract.py
+++ Code/extract.py
@@ -1,12 +1,31 @@
+# Assigmement :
+# 1.  Implement the product_data.csv file from S3 system ( AWS ) rather than extracting it from local
+# 2. Learn what are the different logging level ( there are 8 logging levels ) ( Info, error, fatal....)
+# 3. complete the exception handling for remaining fuctions
+
 import pandas as pd
 import paramiko
 import pytest
 from sqlalchemy import create_engine
 import cx_Oracle
+import logging
+from Config.config import *
 
-oracle_engine = create_engine("oracle+cx_oracle://system:admin@localhost:1521/xe")
-mysql_engine = create_engine("mysql+pymysql://root:Admin%40143@localhost:3308/Retaildwh")
 
+logging.basicConfig(
+    filename='Logs/process.log',  # Name of the log file
+    filemode='a',  # 'a' to append, 'w' to overwrite
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    level=logging.INFO  # Set the logging level
+)
+logger = logging.getLogger(__name__)
+
+
+#oracle_engine = create_engine("oracle+cx_oracle://system:admin@localhost:1521/xe")
+oracle_engine = create_engine(f'oracle+cx_oracle://{ORACLE_USER}:{ORACLE_PASSWORD}@{ORACLE_HOST}:{ORACLE_PORT}/{ORACLE_SERVICE}')
+
+#mysql_engine = create_engine("mysql+pymysql://root:Admin%40143@localhost:3308/Retaildwh")
+mysql_engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}')
 # extrac the data from sales_data_tobe_deleted.csv and write in to staging_sales table in mysql
 
 class DataExtraction:
@@ -17,52 +36,59 @@ class DataExtraction:
             # Establish SSH client connection
             ssh_client = paramiko.SSHClient()
             ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Auto accept unknown keys
-            ssh_client.connect('192.168.0.111', username='etlqalabs', password='root')
+            ssh_client.connect({hostname}, username={username}, password={password})
             # Use SFTP to download the file
             sftp = ssh_client.open_sftp()
-            sftp.get('/home/etlqalabs/sales_data.csv' , 'SourceSystems/sales_data_Linux.csv')
+            sftp.get({remote_file_path} , {local_file_path})
             #print(f"File downloaded successfully from {remote_file_path} to {local_file_path}")
         except Exception as e:
             print(f"An error occurred during SFTP file transfer: {e}")
             raise pytest.fail(f"Test failed due to SSH/SFTP error: {e}")
 
     def extraction_of_sales_data_file(self):
-        print("Data extraction for sales_data_tobe_deleted.csv has started..")
-        df = pd.read_csv("SourceSystems/sales_data_Linux.csv")
-        df.to_sql("staging_sales",mysql_engine,if_exists='replace',index=False)
-        print("Data extraction for sales_data_tobe_deleted.csv has completed..")
+        logger.info("Data extraction for sales_data_tobe_deleted.csv has started..")
+        try:
+            df = pd.read_csv("SourceSystems/sales_data_Linux.csv")
+            df.to_sql("staging_sales",mysql_engine,if_exists='replace',index=False)
+            logger.info("Data extraction for sales_data_tobe_deleted.csv has completed..")
+        except Exception as e:
+            logger.error("Error encountered while sales_data extrcation",e,exc_info=True)
+
 
     def extraction_of_product_data_file(self):
-        print("Data extraction for product_data.csv has started..")
-        df = pd.read_csv("SourceSystems/product_data.csv")
-        df.to_sql("staging_product",mysql_engine,if_exists='replace',index=False)
-        print("Data extraction for product_data.csv has completed..")
+        logger.info("Data extraction for product_data.csv has started..")
+        try:
+            df = pd.read_csv("SourceSystems/product_data.csv")
+            df.to_sql("staging_product",mysql_engine,if_exists='replace',index=False)
+            logger.info("Data extraction for product_data.csv has completed..")
+        except Exception as e:
+            logger.error("Error encountered while product_data extrcation", e, exc_info=True)
 
     def extraction_of_supplier_data_file(self):
-        print("Data extraction for supplier_data.json has started..")
+        logger.info("Data extraction for supplier_data.json has started..")
         df = pd.read_json("SourceSystems/supplier_data.json")
         df.to_sql("staging_supplier",mysql_engine,if_exists='replace',index=False)
-        print("Data extraction for supplier_data.json has completed..")
+        logger.info("Data extraction for supplier_data.json has completed..")
 
     def extraction_of_inventory_data_file(self):
-        print("Data extraction for inventory_data.xml has started..")
+        logger.info("Data extraction for inventory_data.xml has started..")
         df = pd.read_xml("SourceSystems/inventory_data.xml",xpath=".//item")
         df.to_sql("staging_inventoy",mysql_engine,if_exists='replace',index=False)
-        print("Data extraction for inventory_data.xml has completed..")
+        logger.info("Data extraction for inventory_data.xml has completed..")
 
     def extraction_of_stores_data_Oracle_db(self):
-        print("Data extraction for stores data has started..")
+        logger.info("Data extraction for stores data has started..")
         SQLquery = """select * from stores"""
         df = pd.read_sql(SQLquery,oracle_engine)
         df.to_sql("staging_stores",mysql_engine,if_exists='replace',index=False)
-        print("Data extraction for stores data  has completed..")
+        logger.info("Data extraction for stores data  has completed..")
 
 extractRef = DataExtraction()
-extractRef.Sales_Data_From_Linux_Server()
+#extractRef.Sales_Data_From_Linux_Server()
+logger.info("Dtaa extarction processs started...")
 extractRef.extraction_of_sales_data_file()
-'''
 extractRef.extraction_of_product_data_file()
 extractRef.extraction_of_supplier_data_file()
 extractRef.extraction_of_inventory_data_file()
 extractRef.extraction_of_stores_data_Oracle_db()
-'''
+logger.info("Dtaa extarction processs successfully completed...")
---
+++ Code/transform.py
@@ -0,0 +1,102 @@
+
+import pandas as pd
+import paramiko
+import pytest
+from sqlalchemy import create_engine
+import cx_Oracle
+import logging
+from Config.config import *
+
+
+logging.basicConfig(
+    filename='Logs/transformProcess.log',  # Name of the log file
+    filemode='a',  # 'a' to append, 'w' to overwrite
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Log format
+    level=logging.INFO  # Set the logging level
+)
+logger = logging.getLogger(__name__)
+
+
+#mysql_engine = create_engine("mysql+pymysql://root:Admin%40143@localhost:3308/Retaildwh")
+mysql_engine = create_engine(f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}')
+# extrac the data from sales_data_tobe_deleted.csv and write in to staging_sales table in mysql
+
+class DataTranformation:
+
+    def transform_filter_sales_data(self):
+        logger.info("Filter  Transfromatio  has started..")
+        try:
+            query = """select * from staging_sales where sale_date>='2024-09-10'"""
+            df = pd.read_sql(query, mysql_engine)
+            df.to_sql("filtered_sales_data", mysql_engine, if_exists='replace', index=False)
+            logger.info("Filter Transfromation  has completed..")
+        except Exception as e:
+            logger.error("Error encountered while filter transformation",e,exc_info=True)
+
+    def transform_router_sales_data_Low(self):
+        logger.info("Router- LOW  Transfromatio  has started..")
+        try:
+            query = """select * from filtered_sales_data where region='Low'"""
+            df = pd.read_sql(query, mysql_engine)
+            df.to_sql("low_sales", mysql_engine, if_exists='replace', index=False)
+            logger.info("Router- LOW Transfromation  has completed..")
+        except Exception as e:
+            logger.error("Error encountered while Router- LOW transformation",e,exc_info=True)
+
+    def transform_router_sales_data_High(self):
+        logger.info("Router- HIGH  Transfromatio  has started..")
+        try:
+            query = """select * from filtered_sales_data where region='High'"""
+            df = pd.read_sql(query, mysql_engine)
+            df.to_sql("high_sales", mysql_engine, if_exists='replace', index=False)
+            logger.info("Router- HIGH Transfromation  has completed..")
+        except Exception as e:
+            logger.error("Error encountered while Router- HIGH transformation",e,exc_info=True)
+
+    def transform_aggregator_sales_data(self):
+        logger.info("Aggregator  Transfromatio  has started..")
+        try:
+            query = """select product_id,month(sale_date),year(sale_date),sum(quantity*price) as total_sales 
+                        from filtered_sales_data group by product_id,month(sale_date),year(sale_date);"""
+            df = pd.read_sql(query, mysql_engine)
+            df.to_sql("monthly_sales_summary_source", mysql_engine, if_exists='replace', index=False)
+            logger.info("Aggregator Transfromation  has completed..")
+        except Exception as e:
+            logger.error("Error encountered while Aggregator transformation",e,exc_info=True)
+
+    def transform_aggregator_inventory_level(self):
+        logger.info("Aggregator inventory Transfromatio  has started..")
+        try:
+            query = """select store_id,sum(quantity_on_hand) from staging_inventoy group by store_id;"""
+            df = pd.read_sql(query, mysql_engine)
+            df.to_sql("aggregated_inventory_level", mysql_engine, if_exists='replace', index=False)
+            logger.info("Aggregator inventory Transfromation  has completed..")
+        except Exception as e:
+            logger.error("Error encountered while Aggregator inventory transformation",e,exc_info=True)
+
+
+    def transform_joiner_sales_product_store(self):
+        logger.info("joiner_sales_product_store Transfromatio  has started..")
+        try:
+            query = """select fs.sales_id,fs.quantity,fs.sale_date,fs.price,fs.quantity*fs.price as total_sales,
+                        p.product_id,p.product_name,s.store_id,s.store_name
+                        from filtered_sales_data as fs
+                        inner join staging_product as p
+                        inner join staging_stores as s;"""
+            df = pd.read_sql(query, mysql_engine)
+            df.to_sql("sales_with_details", mysql_engine, if_exists='replace', index=False)
+            logger.info("joiner_sales_product_storeTransfromation  has completed..")
+        except Exception as e:
+            logger.error("Error encountered while joiner_sales_product_store transformation",e,exc_info=True)
+
+
+
+transformRef = DataTranformation()
+logger.info("Dtaa Transfromaiton processs started...")
+transformRef.transform_filter_sales_data()
+transformRef.transform_router_sales_data_High()
+transformRef.transform_router_sales_data_Low()
+transformRef.transform_aggregator_sales_data()
+transformRef.transform_aggregator_inventory_level()
+transformRef.transform_joiner_sales_product_store()
+logger.info("Dtaa Transfromaiton processs successfully completed...")
